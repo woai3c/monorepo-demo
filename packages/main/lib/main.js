a0d342c57bf5d58108ec8709fab2b4740e7dfe66
woai3c <411020382@qq.com>
2021-02-26 22:18:07 +0800

    chore: 添加一个 log 语句

--- packages/main/lib/main.js
+++ packages/main/lib/main.js
@@ -4,3 +4,4 @@ const _ = require('lodash')
 
 console.log(add(100, 100) == _.add(100, 100))
 console.log(multiply(100, 100) == _.multiply(100, 100))
+console.log(add(100, 100) == _.multiply(100, 100))
